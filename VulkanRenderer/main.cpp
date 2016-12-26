e46ea91b1003ce9f3c437421d67d3dac748ce012
Christiaan Arnoldus <christiaan.arnoldus@outlook.com>
2016-12-26 19:17:51 +0100

    Create a Vulkan instance

--- VulkanRenderer/main.cpp
+++ VulkanRenderer/main.cpp
@@ -8,8 +8,38 @@ static void error_callback(int error, const char* description)
     throw std::runtime_error(description);
 }
 
+vk::Instance create_instance()
+{
+#if _DEBUG
+    const auto layerCount = 1;
+    const char* layerNames[layerCount] = {"VK_LAYER_LUNARG_standard_validation"};
+#else
+    const auto layerCount = 0;
+    const char **layerNames = nullptr;
+#endif
+
+    const auto extensionCount = 3;
+    const char* extensionNames[extensionCount] = {
+        VK_EXT_DEBUG_REPORT_EXTENSION_NAME,
+        VK_KHR_SURFACE_EXTENSION_NAME,
+        VK_KHR_WIN32_SURFACE_EXTENSION_NAME,
+    };
+
+    auto instance = vk::createInstance(
+        vk::InstanceCreateInfo()
+        .setEnabledLayerCount(layerCount)
+        .setPpEnabledLayerNames(layerNames)
+        .setEnabledExtensionCount(extensionCount)
+        .setPpEnabledExtensionNames(extensionNames)
+    );
+
+    return instance;
+}
+
 int main(int argc, char** argv)
 {
+    auto instance = create_instance();
+
     auto success = glfwInit();
     assert(success);
 
@@ -20,12 +50,20 @@ int main(int argc, char** argv)
     auto window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan renderer", nullptr, nullptr);
     assert(window);
 
+    VkSurfaceKHR surface;
+    auto result = glfwCreateWindowSurface(instance, window, nullptr, &surface);
+    assert(result == VK_SUCCESS);
+
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
     }
 
+    instance.destroySurfaceKHR(surface);
+
     glfwTerminate();
+
+    instance.destroy();
 
     return EXIT_SUCCESS;
 }
